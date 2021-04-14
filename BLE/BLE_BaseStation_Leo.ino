7e25291e2d6875fc5f884ae4717d04599ae929a2
Amir Ghafari <71205869+Amir-Ghafari@users.noreply.github.com>
2021-04-14 09:47:17 -0400

    Add files via upload

--- BLE/BLE_BaseStation_Leo.ino
+++ BLE/BLE_BaseStation_Leo.ino
@@ -8,8 +8,6 @@ void setup()
   Serial.begin(9600);
   Serial.println("HM10 serial started at 9600");
   HM10.begin(9600); // set HM10 serial at 9600 baud rate
-  pinMode(13, OUTPUT); // onboard LED
-  digitalWrite(13, LOW); // switch OFF LED
 
   Serial.begin(115200);
   Serial.setTimeout(50);
@@ -24,7 +22,9 @@ void setup()
 }
 void loop()
 {
-     while (HM10.available() > 0) {
-      String response = HM10.readString();
+  String c;
+  if (HM10.available()) {
+    //What is sent back from the BT module
+    c = HM10.readStringUntil();
 }
 }

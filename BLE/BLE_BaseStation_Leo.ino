dcbd0dd59052d4fdaa25c3a71a8b445662160c09
Amir Ghafari <71205869+Amir-Ghafari@users.noreply.github.com>
2021-04-14 09:30:10 -0400

    Add files via upload

---
+++ BLE/BLE_BaseStation_Leo.ino
@@ -0,0 +1,29 @@
+
+#include <SoftwareSerial.h>
+SoftwareSerial HM10(10, 11); // RX = 10, TX = 11
+char appData;  
+String inData = "";
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("HM10 serial started at 9600");
+  HM10.begin(9600); // set HM10 serial at 9600 baud rate
+  pinMode(13, OUTPUT); // onboard LED
+  digitalWrite(13, LOW); // switch OFF LED
+
+  Serial.begin(115200);
+  Serial.setTimeout(50);
+  HM10.begin(115200); //open BLE connection
+  HM10.setTimeout(50); //set timeout
+  HM10.write("AT+ROLE1");
+  delay(50);
+  bluetooth.write("AT+NOTI1");
+  delay(50);
+  bluetooth.write("AT+CONC40BCB403020");
+  delay(50);
+}
+void loop()
+{
+     while (HM10.available() > 0) {
+      String response = bluetooth.readString();
+}
